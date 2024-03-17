b408db95706acf92a744a3b13185c7e01d6076f0
Kay Tran <kaytran@Kays-MacBook-Pro.local>
2024-03-17 12:23:04 +1000

    Footer with button 'ADD NEW TODO', button has no function

--- milestone1/App.js
+++ milestone1/App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
+let ct = 0;
 export default function App() {
+  const getButtonStyle = ({pressed}) =>
+    pressed ? [styles.pressed, styles.button] : [styles.button];
   return (
     <View style={styles.container}>
+
+      {/* header "My Todo List" */}
       <View style = {styles.header}>
         <Text style = {styles.heading}>My Todo List</Text>
       </View>
+
+      {/* display Todo list */}
       <View style = {styles.main}>
 
       </View>
-      <View style = {styles.footer}></View>
+
+      {/* footer displays add new Todo work */}
+      <View style = {styles.footer}>
+        <Pressable 
+        style = {getButtonStyle} 
+        onPress = {() => {
+          ct++;
+          console.log(`I am clicked ${ct} times`);
+        }}>
+          <Text style = {styles.footerText}>ADD NEW TODO</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -37,8 +55,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
     marginBottom: 10,
-    // borderColor: 'black',
-    // borderWidth: 1,
     
   },
 
@@ -48,5 +64,28 @@ const styles = StyleSheet.create({
 
   footer: {
     flex: 1,
+    borderTopColor: 'black',
+    borderTopWidth: 1,
+    width: '100%',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#5794F0',
+    borderRadius: 5,
+    width: '95%',
+    padding: 10,
+    marginTop: 10,
+  },
+
+  footerText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+
+  },
+  pressed: {
+    opacity: 0.5,
   }
 });
