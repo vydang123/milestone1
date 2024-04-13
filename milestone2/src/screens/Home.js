26f0dc59fcd3c70a8d5d97090ba3d51378b3c350
Kay Tran <kaytran@Kays-MacBook-Pro.local>
2024-04-14 05:14:32 +1000

    successfully retrieving data to Home.js, but there are still some bugs

--- milestone2/src/screens/Home.js
+++ milestone2/src/screens/Home.js
@@ -1,39 +1,97 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import  TButton  from '../components/TButton';
+import { Pressable, StyleSheet, Text, View, FlatList } from 'react-native';
+import {loadData, saveData} from '../datamodel/myData';
+import TButton from '../components/TButton';
 import HeadingTitle from '../components/HeadingTitle';
 import TodoBox from '../components/TodoBox';
+import { useState, useEffect } from 'react';
 
-export default Home = function ({navigation}) {
+export default Home = function ({ navigation}) {
   const navToAddNewTodo = () => navigation.navigate('AddNewTodo');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const firstLoad = async () => {
+      updateTasks()
+    };
+    firstLoad();
+  }, []);
+
+  useEffect (() =>{
+    saveData({tasks})
+  }, [tasks])
+
+  const updateTasks = async () => {
+    const myData = await loadData();
+    setTasks(myData.tasks);
+    console.log('a')
+  };
+  
+  const deleteTask = async (id) => {
+    // Delete the task from the tasks array
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    // Update the saved data
+    await saveData({ tasks: updatedTasks });
+    // Update the state to re-render the component
+    setTasks(updatedTasks);
+  }
+
+ const completeTask = async (id) => {
+    // Mark the task as completed in the tasks array
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, completed: true };
+      }
+      return task;
+    });
+    // Update the saved data
+    await saveData({ tasks: updatedTasks });
+    // Update the state to re-render the component
+    setTasks(updatedTasks);
+  }
+
   return (
     <View style={styles.container}>
-
       {/* header "My Todo List" */}
-      <View style = {styles.header}>
-        <HeadingTitle text = 'My Todo List' />
+      <View style={styles.header}>
+        <HeadingTitle text='My Todo List' />
       </View>
 
       {/* display Todo list */}
-      <View style = {styles.main}>
-        <TodoBox text = 'Buy milk' />
-        <TodoBox text = 'Buy bread' />
-        <TodoBox text = 'Buy eggs' />
+      <View style={styles.main}>
+        <FlatList
+          data={tasks}
+          renderItem={({item}) => (
+            <TodoBox
+              task = {item}
+              title={item.title}
+              description={item.description}
+              cmp={completeTask}
+              del={deleteTask}
+            />
+          )}
+          keyExtractor={t => t.id.toString()}
+        />
       </View>
 
       {/* footer displays add new Todo work */}
-      <View style = {styles.footer}>
-        <TButton icon = 'add-circle' width = '100%' text = 'ADD NEW TODO' onPress = {navToAddNewTodo} />
+      <View style={styles.footer}>
+        <TButton
+          color='green'
+          icon='add-circle'
+          width='100%'
+          text='ADD NEW TODO'
+          onPress={navToAddNewTodo}
+        />
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-
   },
 
   header: {
@@ -46,16 +104,12 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
 
-
   main: {
     flex: 9,
     width: '95%',
     flexDirection: 'column',
     flexWrap: 'wrap',
     paddingTop: 5,
-    // borderColor: 'black',
-    // borderWidth: 1,
-    
   },
 
   footer: {
@@ -66,5 +120,4 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
-
 });
